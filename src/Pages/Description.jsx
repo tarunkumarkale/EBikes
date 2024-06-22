a34f56354b7ff1b3c94b479da6324bc9f320d62a
taurn kale <tkumarkale@gmail.com>
2024-06-22 23:33:11 +0530

    solve related product

--- src/Pages/Description.jsx
+++ src/Pages/Description.jsx
@@ -1,53 +1,49 @@
-
-import React, { useContext, useState,useRef, useEffect } from 'react';
+import React, { useContext, useState, useRef, useEffect } from 'react';
 import { AllDataContext } from '../context/MyContext';
 import { one } from '../constant/Index';
 import ParentRent from '../DesignSection/ParentRent';
 import { useNavigate } from 'react-router-dom';
 
 import CardForRegular from '../DesignSection/CardForRegular';
-import Cardforelectric from '../DesignSection/CardForRegular';
+import Cardforelectric from '../DesignSection/Cardforelectric';
 
 const Description = () => {
   const descriptionRef = useRef(null);
-  let Desc = "Lorem ipsum dolor sit amet consectetur adipisicing elit l Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quibusdam excepturi architecto nesciunt necessitatibus voluptatem quos, placeat perferendis rerum quasi quaerat officia iste impedit similique ex voluptatibus error odit eum itaque optio laboriosam quam culpa. Possimus delectus adipisci perferendis deleniti eos minus quis impedit deserunt expedita a. Error provident ducimus beatae veniam quae sint necessitatibus modi, eius quas nihil alias amet iusto assumenda impedit quo delectus possimus quasi laudantium corrupti perspiciatis totam sunt iste. Accusamus perferendis quia voluptate odio, autem excepturi iure, facere vero molestiae debitis, culpa impedit enim distinctio itaque explicabo facilis blanditiis inventore quibusdam tempora magni accusantium ipsam doloribus. Dolor cupiditate numquam cumque porro cum ipsum a officia hic dolore, possimus sit corrupti animi illo id dolores, sunt deserunt quas repellendus nam nulla iste? Minima recusandae, sunt rem laudantium quod modi sit ab fugiat similique, nulla temporibus dolore consectetur. Expedita doloribus corrupti commodi ullam pariatur iusto exercitationem porro quod ab ad nihil veniam, vitae ducimus quis provident, deserunt suscipit autem amet? Pariatur distinctio ratione voluptatum nesciunt at corrupti tempore enim corporis magni. Inventore nulla minus doloremque nostrum quasi minima quam suscipit provident, iure veritatis, eos hic ipsum error voluptas placeat rerum quo! Nemo, corrupti nam. Laborum quidem cupiditate officiis incidunt libero accusamus nulla dolorum aliquid repellat, ex ullam excepturi architecto explicabo totam ut. Quaerat perferendis atque id harum libero repellat et excepturi, nesciunt, quia ipsam eveniet maiores ipsum obcaecati aperiam pariatur adipisci eligendi corrupti consequatur, laboriosam nulla accusantium? Magnam laborum cupiditate, obcaecati nemo culpa similique aperiam, ea blanditiis recusandae libero vitae pariatur! Similique assumenda dolor exercitationem eos necessitatibus iure quibusdam doloribus alias vitae recusandae iusto, pariatur distinctio velit odio dolore, fugiat excepturi et voluptates rerum quos dignissimos eum, ad architecto? Repudiandae dolor, corrupti dolorum, consequatur iste magni eaque dicta asperiores, veniam ea voluptatum quam? Quibusdam ut magni voluptate et sit, nihil quod sed ducimus, dignissimos molestias provident iusto! Delectus dolor consequatur commodi, dicta neque illo? Expedita nulla iure obcaecati, nostrum quidem porro praesentium facere veritatis aperiam vero adipisci, vel nesciunt enim minima est sed odio reiciendis molestiae, asperiores dolor architecto officia. Optio voluptates sunt tempora laudantium recusandae asperiores! Modi perspiciatis in ducimus nemo expedita perferendis unde libero, rem vitae deserunt nihil veritatis dolorem adipisci consequatur error doloribus ratione fugit, tenetur earum culpa debitis iure et. Error, placeat. Harum ea recusandae blanditiis repellendus asperiores, aut vero dignissimos reprehenderit consectetur labore explicabo vel id alias nam natus dolor iure officiis molestias. Maxime minus odit vel dignissimos delectus distinctio atque error quis at voluptates, vero, architecto ipsam corrupti, numquam ad ipsa accusamus possimus eligendi. Hic modi dolores perspiciatis excepturi repudiandae quam nam ullam porro nobis saepe minima fugiat deserunt, amet ad itaque soluta nesciunt numquam, quaerat eos ut dignissimos. Delectus, quisquam maiores dolorum laudantium non nam, nulla nihil odit provident ea, exercitationem illo? Doloribus, exercitationem nulla rerum dolorem earum pariatur sint maxime cumque numquam laboriosam aperiam esse! Veniam cupiditate soluta delectus, sint adipisci dignissimos vitae minus accusamus consequuntur necessitatibus doloremque error et facilis magni maiores itaque sed. Enim dolores maxime commodi nulla quod consequuntur quisquam esse explicabo ut laudantium quam corporis totam illum voluptatibus est quis unde facilis beatae quibusdam sint atque quae, iusto labore. Suscipit fuga, accusantium vitae praesentium voluptate animi quos deleniti quae quasi modi? Architecto corrupti, ex natus consectetur tenetur doloribus sint nihil sed nulla voluptates nesciunt doloremque officiis adipisci minima quisquam cumque neque quo id voluptatem! Cumque animi corporis corrupti doloribus voluptatum quibusdam quae aliquid totam consectetur harum quasi quis ea, tempora impedit sit veritatis debitis dolorum. Ex, ipsum deleniti? Ipsum accusantium necessitatibus quae delectus atque soluta asperiores dolorum impedit perspiciatis saepe illum blanditiis placeat debitis labore recusandae a officia, non dolore est eum dignissimos tempora? Impedit odit alias, culpa omnis saepe sit libero numquam voluptatem voluptatibus neque consequatur a, laboriosam dolor quibusdam aspernatur aut expedita ad, at ut cum non dolorum. Pariatur et fugit provident laudantium in quam eaque rerum, atque aperiam odio! Doloribus quod odit molestias corrupti modi velit, aliquam minus excepturi ipsum nemo, soluta sit molestiae adipisci vero eligendi dolores assumenda harum perspiciatis accusantium? Repudiandae modi nemo minima, illum exercitationem eius mollitia facere assumenda non. Deserunt facilis molestiae exercitationem cum alias vel, repudiandae veniam itaque, quasi magni accusamus eveniet quia repellat fugiat cumque, optio quod quo illo minus quibusdam odit. Suscipit dolore quibusdam quisquam reprehenderit eum nihil eveniet unde ex nobis dolores tempora ad, porro rerum itaque saepe animi corrupti nam? Tempore amet deserunt, quasi molestias minus debitis sequi dolore saepe voluptate eius ipsa ut nemo rem, est similique molestiae autem quam nostrum laudantium vitae omnis ad aut? Nisi consequatur deserunt, sit repellendus officiis tenetur vitae recusandae, ipsum minus illum quis, nesciunt vero officia velit repellat harum cum suscipit. Quod neque similique iusto vel inventore, atque eligendi sapiente! Sed quas quia laboriosam. Quaerat aliquam aspernatur, eveniet corrupti assumenda sunt facilis sit minima tempore fugiat repellendus ipsam. Eveniet tenetur rerum, ipsam labore doloribus corrupti odit quisquam doloremque numquam beatae fugiat assumenda error consequatur qui suscipit, dignissimos iure nobis architecto ullam nesciunt cupiditate natus facere. Est omnis hic consectetur rerum ullam recusandae, error, asperiores unde pariatur maxime nemo aspernatur dicta illum ab velit sed totam delectus quam? Accusamus voluptas necessitatibus nostrum distinctio. Delectus sit ea omnis odio perspiciatis eius cupiditate! Sit at qui provident aperiam doloremque. Rerum eveniet necessitatibus architecto deleniti sunt reiciendis, commodi quia excepturi culpa distinctio praesentium quas repellat optio consequuntur cum itaque laudantium illo perferendis sint provident explicabo modi veritatis cumque a. Similique voluptatum sequi officia neque est amet velit, corporis et ipsum labore omnis nemo, eligendi fugiat sit quidem saepe recusandae temporibus cupiditate eaque blanditiis consequatur optio. Nam magnam nostrum numquam quas sequi consequatur repudiandae nisi non debitis commodi! Nam quisquam veniam molestiae ex officia minus ea corrupti, laborum atque quaerat modi dolore deleniti blanditiis consequatur esse fugiat mollitia. Amet, laudantium impedit. "; // Your description goes here
-  let final_Desc = Desc.slice(0, 800); 
+  let Desc = "Lorem ipsum dolor sit amet consectetur adipisicing elit..."; // Your description goes here
+  let final_Desc = Desc.slice(0, 800);
 
-  const {RentFormDataimage,RentFormDataprice,RentFormDataNAME,electricComponets}=useContext(AllDataContext)
+  const { RentFormDataimage, RentFormDataprice, RentFormDataNAME, electricComponets } = useContext(AllDataContext);
   
-  const navigate=useNavigate()
+  const navigate = useNavigate();
 
-  const navigatetoRENTFORM=()=>{
-    navigate('/RentForm')
+  const navigateToRENTFORM = () => {
+    navigate('/RentForm');
   }
 
+  const [renderRelatedProduct, setRenderRelatedProduct] = useState(null);
 
-const [renderRelatedProduct, setRenderRelatedProduct]=useState(null)
+  let first = RentFormDataimage ? RentFormDataimage : one;
+  let second = RentFormDataNAME ? RentFormDataNAME : 'stride';
+  let third = RentFormDataprice ? RentFormDataprice : "Rs:4200";
 
-let first= RentFormDataimage? RentFormDataimage: one
-let second= RentFormDataNAME? RentFormDataNAME: 'stride'
-let third= RentFormDataprice? RentFormDataprice: "Rs:4200"
+  const scrollToTop = () => {
+    if (descriptionRef.current) {
+      descriptionRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
-
-
-const scrollToTop = () => {
-  if (descriptionRef.current) {
-    descriptionRef.current.scrollIntoView({ behavior: 'smooth' });
-  }
-};
-
-console.log(electricComponets)
-useEffect(() => {
-  if (electricComponets === "Regular") {
-    setRenderRelatedProduct(<CardForRegular scrollToTop={scrollToTop} />);
-  } else if (electricComponets === "electric") {
-    setRenderRelatedProduct(<Cardforelectric scrollToTop={scrollToTop} />);
-  } else {
-    setRenderRelatedProduct(<ParentRent scrollToTop={scrollToTop} />);
-  }
-}, [renderRelatedProduct,electricComponets]);
-
+  console.log(electricComponets);
   
+  useEffect(() => {
+    if (electricComponets === "Regular") {
+      setRenderRelatedProduct(<CardForRegular scrollToTop={scrollToTop} />);
+    } else if (electricComponets === "electric") {
+      setRenderRelatedProduct(<Cardforelectric scrollToTop={scrollToTop} />);
+    } else {
+      setRenderRelatedProduct(<ParentRent scrollToTop={scrollToTop} />);
+    }
+  }, [electricComponets]); // Dependency array fixed to only include electricComponets
+
   return (
     <div className='bg-white' ref={descriptionRef}>
       <div className="flex flex-col md:flex-row">
@@ -61,51 +57,41 @@ useEffect(() => {
             <p className="text-lg font-semibold">Rs:{third}</p>
           </div>
 
-       
-
           <div className="flex items-center gap-9 mt-3">
-            <button className="px-4 py-2 bg-blue-400 text-white hover:bg-blue-800 transition duration-300" onClick={navigatetoRENTFORM}>{first?'Rent Now':'Buy Now'}</button>
+            <button className="px-4 py-2 bg-blue-400 text-white hover:bg-blue-800 transition duration-300" onClick={navigateToRENTFORM}>{first ? 'Rent Now' : 'Buy Now'}</button>
             <button className="px-4 py-2 bg-black text-white hover:bg-white hover:text-black transition duration-300">Add to Cart</button>
           </div>
-        
-<div>
-          <p className='uppercase font-bold'>Total Items in Cart: <span id="total-items">0</span></p>
-          <br />
-          <p className='text-xl'>Brakes: The bicycle is equipped with dual disc brakes that provide superior stopping power and control, ensuring safety even in wet conditions.</p>
-<p className='text-xl'>Gears: Featuring a 21-speed Shimano gear system, this bike allows for smooth and precise shifting, making it suitable for various terrains.</p>
-<br />
-<p className='text-xl'>Tires: The bike comes with 29-inch all-terrain tires that offer excellent grip and stability, whether you're riding on pavement or dirt trails.</p>
-<br />
-<p className='text-xl'>Saddle: The ergonomic saddle is designed for comfort during long rides, with ample padding and adjustable height to fit different riders.</p>
-<br />
-<p className='text-xl'>Suspension: Equipped with a front suspension fork that absorbs shocks from rough terrain, providing a smoother and more comfortable ride.</p>
-<br />
-<p className='text-xl'>Handlebars: The bike features flat handlebars that offer a more upright riding position, reducing strain on the back and shoulders.</p>
-<br />
-<p className='text-xl'>Accessories: Comes with essential accessories like a water bottle holder, kickstand, and reflectors for added convenience and safety.</p>
 
+          <div>
+            <p className='uppercase font-bold'>Total Items in Cart: <span id="total-items">0</span></p>
+            <br />
+            <p className='text-xl'>Brakes: The bicycle is equipped with dual disc brakes that provide superior stopping power and control, ensuring safety even in wet conditions.</p>
+            <p className='text-xl'>Gears: Featuring a 21-speed Shimano gear system, this bike allows for smooth and precise shifting, making it suitable for various terrains.</p>
+            <br />
+            <p className='text-xl'>Tires: The bike comes with 29-inch all-terrain tires that offer excellent grip and stability, whether you're riding on pavement or dirt trails.</p>
+            <br />
+            <p className='text-xl'>Saddle: The ergonomic saddle is designed for comfort during long rides, with ample padding and adjustable height to fit different riders.</p>
+            <br />
+            <p className='text-xl'>Suspension: Equipped with a front suspension fork that absorbs shocks from rough terrain, providing a smoother and more comfortable ride.</p>
+            <br />
+            <p className='text-xl'>Handlebars: The bike features flat handlebars that offer a more upright riding position, reducing strain on the back and shoulders.</p>
+            <br />
+            <p className='text-xl'>Accessories: Comes with essential accessories like a water bottle holder, kickstand, and reflectors for added convenience and safety.</p>
+          </div>
         </div>
       </div>
-</div>
-      <div className='flex justify-start flex-col  p-2  sm:p-20  m-0 sm:m-6'>
+      <div className='flex justify-start flex-col p-2 sm:p-20 m-0 sm:m-6'>
         <h1 className='text-4xl m-2 w-[20%]'>Description</h1>
-        <p className='text-base border border-gray-500 p-5  '>{final_Desc}</p>
+        <p className='text-base border border-gray-500 p-5'>{final_Desc}</p>
       </div>
 
       {/* Cards of related products */}
-      <div className='flex justify-center items-center '>
-      <h1 className='text-center text-2xl  scale-125 font-extrabold underline'>Related Products </h1>
+      <div className='flex justify-center items-center'>
+        <h1 className='text-center text-2xl scale-125 font-extrabold underline'>Related Products</h1>
       </div>
-      <div className="flex  flex-wrap justify-around mb-40 mx-2 mt-2" >
-   
-      {renderRelatedProduct}
-      
+      <div className="flex flex-wrap justify-around mb-40 mx-2 mt-2">
+        {renderRelatedProduct}
       </div>
-
-     
-
-
-
     </div>
   );
 }
