36ae5c2d38bfaa4fa37c8919698501ab80e01a01
taurn kale <tkumarkale@gmail.com>
2024-06-24 23:40:52 +0530

    start working on sidebar data

--- src/DesignSection/Sidebar.jsx
+++ src/DesignSection/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { IoClose } from "react-icons/io5";
+import Sidebardata from "./Sidebardata";
 
 const Sidebar = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -12,16 +13,9 @@ const Sidebar = () => {
     <div className="flex">
       {/* Sidebar */}
       <div className={`bg-gray-200 h-screen mt-16 w-64 fixed top-0 left-0 transition-transform duration-300 transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'}`}>
-        {/* Sidebar Content */}
-        <div className="p-4">
-          <h2 className="text-lg font-semibold mb-4">Sidebar Content</h2>
-          <ul>
-            <li className="mb-2"><a href="#" className="text-blue-500 hover:text-blue-700">Link 1</a></li>
-            <li className="mb-2"><a href="#" className="text-blue-500 hover:text-blue-700">Link 2</a></li>
-            <li className="mb-2"><a href="#" className="text-blue-500 hover:text-blue-700">Link 3</a></li>
-            {/* Add more sidebar links as needed */}
-          </ul>
-        </div>
+        
+        <Sidebardata/>
+      
         {/* Close button */}
         <button className="absolute top-4 right-4" >
           <IoClose className="h-6 w-6 text-gray-600" onClick={toggleSidebar}  />
---
+++ src/DesignSection/Sidebardata.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Sidebardata = () => {
+  return (
+    <div>
+  <div className="p-4">
+          <h2 className="text-lg font-semibold mb-4">Sidebar Content</h2>
+          <ul>
+            <li className="mb-2"><a href="#" className="text-blue-500 hover:text-blue-700">Link 1</a></li>
+            <li className="mb-2"><a href="#" className="text-blue-500 hover:text-blue-700">Link 2</a></li>
+            <li className="mb-2"><a href="#" className="text-blue-500 hover:text-blue-700">Link 3</a></li>
+            
+          </ul>
+        </div>      
+    </div>
+  )
+}
+
+export default Sidebardata
